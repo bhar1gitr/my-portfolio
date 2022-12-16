02257e7db61a5f122ddf62207294b5ee2d99bdc9
Bharat Sharma <97161064+bhar1gitr@users.noreply.github.com>
2022-12-16 15:28:25 +0530

    Update user.js

--- server/routes/user.js
+++ server/routes/user.js
@@ -4,5 +4,8 @@ const router = express.Router();
 const userController = require('../controllers/userController');
 
 router.post("/info",userController.send);
+router.get("/bharat", (req,res)=>{
+    return res.status(200).json({message: "Hello World"});
+});
 
 module.exports = router;
