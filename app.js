d74113fcbfede126394e497cbaa35f011acf3e7a
Bharat Sharma <97161064+bhar1gitr@users.noreply.github.com>
2022-12-30 20:45:35 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,18 +1,37 @@
 const express = require("express");
 const hbs = require('hbs');
-const mysql = require("mysql");
+const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const app = express();
 const path = require('path');
-const fileUpload = require('express-fileupload');
 require('dotenv').config();
-const port = 3000 || process.env.PORT;
 
-// BodyParser
-app.use(bodyParser.urlencoded({ extended : true }))
-
+app.use(bodyParser.urlencoded({extended:true}));
 // Setting view engiine to hbs
 app.set("view engine","hbs");
+
+mongoose.set('strictQuery', true);
+mongoose.connect(`${process.env.DB_LINK}`)
+.then(() => console.log("Connected to atlas!!!"))
+.catch((err) => console.log(err));
+
+const userSchema = {
+  name : String,
+  email : String,
+  msg : String
+}
+
+const user = mongoose.model("user",userSchema);
+
+app.post("/",(req,res)=>{
+    let newUser = new user({
+      name : req.body.name,
+      email : req.body.email,
+      msg : req.body.msg
+    })
+    newUser.save();
+    res.render("index");
+})
 
 // Setting default views to templates
 const templatePath = path.join(__dirname,"/templates/views");
@@ -25,44 +44,16 @@ hbs.registerPartials(partialsPath);
 // For static paths
 const staticPath = path.join(__dirname,"public");
 app.use(express.static(staticPath));
-const staticImagePath = path.join(__dirname,"public/images");
-app.use(express.static(staticImagePath));
 
-// Routes
-const routes = require("./server/routes/user");
-app.use("/",routes)
+// app.get("/index",(req,res)=>{
+//        res.render("index");
+// })
 
-// Database
-const pool = mysql.createPool({
-  connectionLimit : 100,
-  host : process.env.DB_HOST,   
-  user : process.env.DB_USER,  
-  password : process.env.DB_PASS,   
-  database : process.env.DB_NAME    
-});
 
-pool.getConnection((err,connection)=>{
-  if(err) throw err;
-  console.log("Connected as ID " + connection.threadId);
-});
-
-// var small_data = {
-//   data: [
-//       {url: "https://images.unsplash.com/photo-1670431757843-e8fd3f24490f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80", content: "This awesom!!!" },
-//       {url: "https://images.unsplash.com/photo-1670431757843-e8fd3f24490f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80", content: "This awesom!!!" },
-//       {url: "https://images.unsplash.com/photo-1670431757843-e8fd3f24490f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80", content: "Nice!!!" },
-//       {url: "https://images.unsplash.com/photo-1670431757843-e8fd3f24490f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80", content: "This awesom!!!" },
-//       {url: "https://images.unsplash.com/photo-1670431757843-e8fd3f24490f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80", content: "Nice!!!" },
-//       {url: "https://images.unsplash.com/photo-1670431757843-e8fd3f24490f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80", content: "This awesom!!!" },
-//       {url: "https://images.unsplash.com/photo-1670431757843-e8fd3f24490f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80", content: "Nice!!!" },
-//       {url: "https://images.unsplash.com/photo-1670431757843-e8fd3f24490f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80", content: "This awesom!!!" },
-//       {url: "https://images.unsplash.com/photo-1670431757843-e8fd3f24490f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80", content: "Nice!!!" },
-//   ]
-// };
 
 app.get("/my-projects",(req,res)=>{
   res.render("projects",small_data)})
 
-app.listen(port,()=>{
-  console.log(`Listening to the port ${port}`);
-});
+  app.listen(6969,()=>{
+    console.log("Running on 6969");
+  })
